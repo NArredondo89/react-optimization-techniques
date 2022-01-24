d59ef42477e710738577424b051a5c24c1cfccf4
Nick <86270545+NArredondo89@users.noreply.github.com>
2022-01-24 15:40:54 -0800

    react useMemo, useCallback

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useCallback } from 'react';
+
+import './App.css';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('app running');
+
+  const toggleParagraph = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
